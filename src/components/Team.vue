8b9a65ceedaec6b713404fa1e94f88b083b1fb7d
josefj <josef@polynom.com>
2019-09-14 18:26:47 +0200

    Social icons added

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -9,9 +9,18 @@
         <img :src="i.pic"/>
       </div>
       <div class="nine columns">
-        <h3 class="left"> {{ i.firstName }} {{ i.lastName }} </h3>
-
+        <div class="first">
+          <h3 class="left name"> {{ i.name }} </h3>
+          <div class="social">
+            <a v-if="i.github !== null" :href="i.github" class="soc-logo"> <img src="../assets/gh.png"> </a>
+            <a v-if="i.twitter !== null" :href="i.twitter" class="soc-logo"> <img src="../assets/tw.png"> </a>
+            <a v-if="i.linkedin !== null" :href="i.linkedin" class="soc-logo"> <img src="../assets/ln.svg"> </a>
+            <a v-if="i.peepeth !== null" :href="i.peepeth" class="soc-logo"> <img src="../assets/pe.png"> </a>
+            <a v-if="i.keybase !== null" :href="i.keybase" class="soc-logo"> <img src="../assets/kb.png"> </a>
+          </div>
+        </div>
         <h4 class="left"> {{ i.role }} </h4>
+        <div class="links"> </div>
         <p class="left"> {{ i.bio }} </p>
       </div>
     </div>
@@ -59,6 +68,51 @@ h3, h4 {
   max-width: 100%;
 }
 
+.profile {
+  margin-top: 20px;
+}
+
+p {
+  text-align: justify;
+}
+
+  .first {
+    display: flex;
+    justify-content: space-between;
+  }
+  .social {
+    /*margin-bottom: 10px;*/
+  }
+
+  .soc-logo {
+    display: inline;
+    margin-left: 20px;
+  }
+
+  .soc-logo img {
+    width: 25px;
+    height: 25px;
+    margin-top: 10px;
+  }
+
+  .name {
+    /*display: flex;*/
+  }
+
+@media (max-width: 750px) {
+  .first {
+    display: inline;
+  }
+  .social {
+    text-align: left;
+    margin-bottom: 8px;
+  }
+
+  .soc-logo:first-child {
+    margin-left: 0px;
+  }
+}
+
 @media (max-width: 550px) {
   .pic img {
     max-width: 50%;
@@ -67,14 +121,9 @@ h3, h4 {
   h3, h4 {
     text-align: center;
   }
-}
-
-.profile {
-  margin-top: 20px;
-}
-
-p {
-  text-align: justify;
+  .social {
+    text-align: center;
+  }
 }
 
 </style>
